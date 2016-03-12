ea915ff26851ff43b87f96f2565d9e4732a87560
Derrick Schneider <derrick.schneider@opower.com>
2016-03-11 22:38:32 -0800

    Clean up code dramatically, rename to PRisk.
    This puts what was a bunch of thrown-together JavaScript
    into a more standard JavaScript syntax. It also does some
    refactoring as part of that effort

--- overall_risk.js
+++ overall_risk.js
@@ -1,200 +1,321 @@
-/** Does various queries to determine overall risk for a given pull request. */
+/** Provides risk heuristics for a given pull request. */
 
-var URL_SLASH = "/";
-var RESULTS_ID = "pr-buddy-overall-risk-results";
-var MINIMAL_SEARCH_RESULTS = "&per_page=1";
-var AUTHOR_NEWNESS_ID = "pr-buddy-author-newness-risk";
-var AUTHOR_REPUTATION_ID = "pr-buddy-author-reputation";
-var AVG_MAX_COMPLEXITY_ID = "pr-buddy-avg-max-complexity";
-var NUM_FILES_ID = "pr-buddy-num-files";
-var TOTAL_CHANGES_ID = "pr-buddy-total-changes";
-var FIRST_NON_WHITESPACE_REGEX = /[^\s]/;
+var prisk = {
 
-var FIELD_TO_DESCRIPTION = {};
-FIELD_TO_DESCRIPTION[AUTHOR_NEWNESS_ID] = "Author newness risk";
-FIELD_TO_DESCRIPTION[AVG_MAX_COMPLEXITY_ID] = "Avg. max complexity risk";
-FIELD_TO_DESCRIPTION[NUM_FILES_ID] = "Number of files risk";
-FIELD_TO_DESCRIPTION[TOTAL_CHANGES_ID] = "Total changes risk";
+  /** This serves as the main entry point for this object.
+   *  The page calls this to start the risk assessment.
+   *
+   *  @public
+   */
+  createRiskAssessment: function() {
+    prisk.configureUI_();
 
-function getResultsTable() {
-  return document.getElementById(RESULTS_TABLE_ID);
-};
+    prisk.loadPRData_(prisk.getPRAPIURL_(window.location.href), function(prData) {
 
-/** Split a URL based on the / character. */
-function splitURL(url) {
-  return url.split(URL_SLASH);
-};
+      // fill in some convenience info from the data
+      prisk.pr_repo =  prData.base.repo.name;
+      prisk.pr_org = prData.base.repo.owner.login;
+      prisk.mergedSearchQuery = 'type:pr is:merged repo:' + prisk.pr_org + prisk.constants_.URL_SLASH + prisk.pr_repo;
 
-/** Replaces the first child of the given element with a text
- *  node containing the given text
- */
-function replaceFirstChildWithText(element, newText) {
-  element.replaceChild(document.createTextNode(newText), element.firstChild);
-}
+      // fill in the "total changes" risk value, which is just the sum of additions/deletions
+      prisk.setMetricField_(prisk.constants_.FIELD_TO_DESCRIPTION.TOTAL_CHANGES,
+                              prData.additions + prData.deletions);
+      // changed files
+      prisk.setMetricField_(prisk.constants_.FIELD_TO_DESCRIPTION.NUM_FILES, prData.changed_files);
 
-/** Create a map() equivalent for HTMLCollections.
- *  Returns an array of the results.
-*/
-function htmlCollectionMap(collection, mapFunction) {
-  var returnValue = [];
+      prisk.showAuthorNewness_(prData.user.login);
+    });
 
-  for (var collectionIndex = 0; collectionIndex < collection.length; collectionIndex++) {
-    var mapResult = mapFunction(collection.item(collectionIndex));
-    returnValue.push(mapResult);
-  }
-  return returnValue;
-}
+    // with that kicked off, calculate the average max complexity
+    prisk.setMetricField_(prisk.constants_.FIELD_TO_DESCRIPTION.AVG_MAX_COMPLEXITY,
+      prisk.calculateAverageMaxComplexity_());
+  },
 
-/** Calculates the github API root based on the incoming URL */
-function getAPIRoot(url) {
-  var urlParts = splitURL(url);
-  return [urlParts[0] + URL_SLASH, urlParts[2], "api", "v3"].join(URL_SLASH);
-}
+  /** Calculate the average max complexity of the PR
+   *  by trawling through the diff divs and looking at the
+   * indentation of the code lines.
+   *
+   * @private
+   * @return {Float} the average max complexity of all the diffs.
+   */
+   calculateAverageMaxComplexity_: function() {
+     var filesBucket = document.getElementById('files');
+     var fileDiffs = filesBucket.getElementsByClassName('file');
 
-/** Figure out the github API URL from the tab URL */
-function getPRAPIURL(url) {
-  var url_parts = splitURL(url);
-  return [getAPIRoot(url), "repos", url_parts[3], url_parts[4], "pulls", url_parts[6]].join(URL_SLASH);
-}
+     var maxComplexities = prisk.htmlCollectionMap_( fileDiffs, function(file) {
+       return prisk.getComplexityForDiffDiv_(file);
+     });
 
-/** Given a page URL, return the search api root. */
-function getSearchAPIURL(url) {
-  return [getAPIRoot(url), "search", "issues"].join(URL_SLASH);
-}
+     var nonZeroComplexities = maxComplexities.filter( function(complexity) { return complexity != 0;} );
+     console.log(nonZeroComplexities);
+     var nonZeroComplexitiesSum = nonZeroComplexities.reduce( function( prev, current, _index, _array) { return prev + current; }, 0 );
+     console.log(nonZeroComplexitiesSum);
+     return nonZeroComplexitiesSum/nonZeroComplexities.length;
+   },
 
-/** Get PR Blob */
-function getPR(url, callback) {
-  var prAPI = getPRAPIURL(url);
-  var xhr = new XMLHttpRequest();
-  xhr.onload = function() {
-    callback(JSON.parse(xhr.response));
-  }
-  xhr.open('GET', prAPI);
-  xhr.send();
-}
+   /** Given a diff representing a file, return the maximum complexity
+    * as defined by the number of unique indent levels, which is a stand-in
+    * for McCabe complexity.
+    * See: Reading Beside the Lines: Indentation as a Proxy for Complexity Metrics
+    * Hindle, Godfrey, Holt.
+    *
+    * @private
+    * @param {Element} the diff for a given file
+    * @return {Integer} the maximum complexity within this diff.
+    */
+    getComplexityForDiffDiv_(diff) {
+      var additions = diff.getElementsByClassName('blob-code blob-code-addition');
+      var individualIndents = prisk.htmlCollectionMap_(additions, function(addition) {
+        var codeSegment = addition.getElementsByClassName('blob-code-inner').item(0).firstChild.textContent.substring(1);
+        var firstNonWhitespace = codeSegment.search(prisk.constants_.FIRST_NON_WHITESPACE_REGEX);
+        if (firstNonWhitespace == -1) {
+          // if no non-whitespace characters were found, we just want the length of the all-whitespace string
+          // it is sometimes the case that there are whitespace characters in the first text node, sometimes not.
+          firstNonWhitespace = codeSegment.length;
+        }
+        return firstNonWhitespace;
+      });
 
-/** Determine the PR's author and send that back to the callback. */
-function getPRAuthor(url, callback) {
+      // now get the unique values of indents within this diff. the length of that
+      // is the maximum complexity of the diff's additions
+      var uniqueIndents = [];
+      individualIndents.forEach( function(indent) {
+        // use "" + indent because "number".toString creates an index into a sparse array
+        // even though "number" is a string; it's coerced to an Integer
+        uniqueIndents[" " + indent] = true;
+      });
+      return Object.keys(uniqueIndents).length;
+    },
 
-  getPR(url, function(prData) {
-    callback(prData["user"]["login"]);
-  });
-}
+   /** Given an HTMLCollection, apply the map function to each element
+    *
+    * @private
+    * @param {HTMLCollection} the collection of HTML elements to traverse
+    * @param {Function} the mapping function to apply to each item
+    * @return {Array} the mapped values from the collection
+    */
+   htmlCollectionMap_: function(collection, mapFunction) {
+     var returnValue = [];
 
-/** Generate a query string for closed PRs in the org and repo. */
-function getClosedPRQueryString(org, repo) {
-  return ["type:pr", "is:merged", ("repo:" + org + URL_SLASH + repo) ].join(" ");
-}
+     for (var collectionIndex = 0; collectionIndex < collection.length; collectionIndex++) {
+       var mapResult = mapFunction(collection.item(collectionIndex));
+       returnValue.push(mapResult);
+     }
+     return returnValue;
+   },
 
-/** Get the total number of merged PRs for the repo and send that to a callback. */
-function getTotalPRCountForRepo(documentUrl, callback) {
-  var urlParts = splitURL(documentUrl);
-  var queryString = getClosedPRQueryString(urlParts[3], urlParts[4]);
-  var searchApiRoot = getSearchAPIURL(documentUrl);
+  /** Sets the metric for an author's newness, which is defined as 100 -
+   *  the percentage of merged PRs authored by this person.
+   *  The subtraction is to make the logic consistent for calculating
+   *  low, medium, or high risk. A newness value of 0 means the author
+   *  wrote everything in the repo. A newness value of 100 means the
+   *  author has never written anything for this repo.
+   *
+   * @private
+   * @param {String} the username of this PR's author
+   */
+  showAuthorNewness_: function(login) {
+     var authorSearchUrl = prisk.getSearchAPIURL_(document.location.href) + '?q=' +
+       encodeURIComponent(prisk.mergedSearchQuery + ' author:' + login) + prisk.constants_.MINIMAL_SEARCH_RESULTS;
 
-  var xhr = new XMLHttpRequest();
-  xhr.onload = function() {
-    callback(JSON.parse(xhr.response)["total_count"]);
-  }
+     prisk.loadJsonFromUrl_(authorSearchUrl, function(authorPRSearchData) {
+       var authoredCount = authorPRSearchData.total_count;
 
-  xhr.open('GET', searchApiRoot + "?q=" + encodeURIComponent(queryString) + MINIMAL_SEARCH_RESULTS);
-  xhr.send();
-}
+       // that gets us the author's PRs, but we need to compare to the total PRs
+       // for the repo with another search and then divide
+       var allRepoPRsUrl = prisk.getSearchAPIURL_(document.location.href) + '?q=' +
+         encodeURIComponent(prisk.mergedSearchQuery) + prisk.constants_.MINIMAL_SEARCH_RESULTS;
 
-/** Figures out the percentage of PRs this PR's author has had merged. */
-function getAuthorPRPercentage(url, callback) {
-  getPRAuthor(url, function(author) {
-    console.log("author: " + author);
-    getTotalPRCountForRepo(url, function(totalPRCount) {
-      console.log("total PRs: " + totalPRCount);
-      var urlParts = splitURL(url);
-      var queryString = getClosedPRQueryString(urlParts[3], urlParts[4]) + " author:" + author;
-      console.log(queryString);
+       prisk.loadJsonFromUrl_(allRepoPRsUrl, function(allPRSearchData) {
+         var totalCount = allPRSearchData.total_count;
+         prisk.setMetricField_(prisk.constants_.FIELD_TO_DESCRIPTION.AUTHOR_NEWNESS, 100 - ((authoredCount/totalCount) * 100));
+       });
+     });
+  },
 
+  /** Sets the value of the specified metric field.
+   *
+   * @private
+   * @param {String} the ID of the field to set
+   * @param {Object} the value to put into that field
+   */
+ setMetricField_: function(metric, value) {
+     var field = document.getElementById(metric.id);
+     if (field == null) {
+       console.log('field ' + metric.id + ' not found');
+       return;
+     }
+
+     var riskAssessment = 'LOW';
+     if (value > metric.goodValue && value <= metric.warnValue) {
+       riskAssessment = 'MEDIUM';
+     }
+
+     if (value > metric.warnValue) {
+       riskAssessment = 'HIGH';
+     }
+     console.log('risk value for ' + metric.id + ': ' + value.toString());
+
+     field.replaceChild(document.createTextNode(riskAssessment), field.firstChild);
+  },
+
+  /** Loads the PR JSON from the given URL.
+   *
+   * @private
+   * @param {String} the API url from which to fetch the PR JSON
+   * @param {Function} a callback function that takes the PR data JSON
+   */
+  loadPRData_: function(prUrl, prHandlerFunction) {
+     prisk.loadJsonFromUrl_(prUrl, function(payload) {
+       prHandlerFunction(payload);
+     });
+  },
+
+   /** Fetches the passed-in URL, parses it to JSON, and then passes that
+    *  JSON to the passed-in function.
+    *
+    * @private
+    * @param {String} the URL to fetch from
+    * @param {Function} the function to call when the data loads. Takes the JSON payload.
+    */
+  loadJsonFromUrl_: function(url, payloadHandlerFunction) {
       var xhr = new XMLHttpRequest();
       xhr.onload = function() {
-        console.log("author PRs: " + JSON.parse(xhr.response)["total_count"]);
-        callback((JSON.parse(xhr.response)["total_count"]/totalPRCount) * 100);
-      }
-
-      // no need to get lots of query results: we only care about the top-level count
-      xhr.open('GET', getSearchAPIURL(url) + "?q=" + encodeURIComponent(queryString) + MINIMAL_SEARCH_RESULTS);
+        payloadHandlerFunction(JSON.parse(xhr.response));
+      };
+      xhr.open('GET', url);
       xhr.send();
-    });
-  });
-}
+  },
 
-/** Figures out the API URL for the given pull request. */
-var headerPane = document.getElementById("partial-discussion-header");
+  /** Retrieve the API URL to use for this PR, based
+   *  on the page's URL.
+   *
+   * @private
+   * @param {String} the browser URL from which to derive the API url
+   * @return {String} the API URL for this PR.
+   */
+  getPRAPIURL_: function(browserUrl) {
+    var url_parts = prisk.splitUrl_(browserUrl);
+    return [prisk.getAPIRootURL_(browserUrl), 'repos', url_parts[3], url_parts[4],
+           'pulls', url_parts[6]].join(prisk.constants_.URL_SLASH);
+  },
 
-var results = document.createElement("table");
-results.id = RESULTS_ID;
+   /** Get the search API URL as derived from the passed-in URL.
+    *
+    * @private
+    * @param {String} the base URL of the page
+    * @return {String} the search API base.
+    */
+  getSearchAPIURL_: function(documentURL) {
+    return [prisk.getAPIRootURL_(documentURL), 'search', 'issues'].join(prisk.constants_.URL_SLASH);
+  },
 
-Object.keys(FIELD_TO_DESCRIPTION).forEach( function(item) {
-  var tr = document.createElement("tr");
-  var descTd = document.createElement("td");
-  descTd.appendChild(document.createTextNode(FIELD_TO_DESCRIPTION[item]));
+   /** Determines the API base URL given the current browser URL.
+    *
+    * @private
+    * @param {String} the window's URL
+    * @return {String} the base of the API URL, based on the browser URL
+    */
+  getAPIRootURL_: function(browserUrl) {
+    var urlParts = prisk.splitUrl_(browserUrl);
+    // the extra URL_SLASH here is to handle the two slashes after the protocol
+    return [urlParts[0] + prisk.constants_.URL_SLASH, urlParts[2],
+           'api', 'v3'].join(prisk.constants_.URL_SLASH);
+  },
 
-  var valueTd = document.createElement("td");
-  valueTd.id = item;
-  valueTd.appendChild(document.createTextNode("Loading"));
 
-  tr.appendChild(descTd);
-  tr.appendChild(valueTd);
-  results.appendChild(tr);
-});
-headerPane.appendChild(results);
+  /** This configures the dom elements in the UI.
+   *
+   * @private
+   */
+  configureUI_: function() {
+     var headerPane = document.getElementById(prisk.getPRPanelId_());
 
-// fill in author "newness"
-getAuthorPRPercentage(document.location.href, function(authorPercentage) {
-  var resultsCell = document.getElementById(AUTHOR_NEWNESS_ID);
-  replaceFirstChildWithText(resultsCell, authorPercentage.toString());
-});
+     var resultsTable = document.createElement('table');
+     resultsTable.id = prisk.constants_.RESULTS_ID;
 
-// fill in "avg max complexity"
-// todo: this code is hard to understand and obtuse
-//   make cleaner and more clear
-var filesBucket = document.getElementById('files');
-var fileDiffs = filesBucket.getElementsByClassName('file');
+     Object.keys(prisk.constants_.FIELD_TO_DESCRIPTION).forEach( function(item) {
+       var tr = document.createElement('tr');
+       var descTd = document.createElement('td');
+       descTd.appendChild(document.createTextNode(prisk.constants_.FIELD_TO_DESCRIPTION[item].description));
 
-// fill in num files while we're at it, since the information we need is right there
-replaceFirstChildWithText(document.getElementById(NUM_FILES_ID), fileDiffs.length.toString());
+       var valueTd = document.createElement('td');
+       valueTd.id = prisk.constants_.FIELD_TO_DESCRIPTION[item].id;
+       valueTd.appendChild(document.createTextNode('Loading'));
 
-var maxComplexities = htmlCollectionMap( fileDiffs, function(file) {
-  // the given diff represents an entire file, so iterate over additions
-  var additions = file.getElementsByClassName('blob-code blob-code-addition');
-  var individualIndents = htmlCollectionMap(additions, function(addition) {
-      var codeSegment = addition.getElementsByClassName('blob-code-inner').item(0).firstChild.textContent.substring(1);
-      var firstNonWhitespace = codeSegment.search(FIRST_NON_WHITESPACE_REGEX);
-      if (firstNonWhitespace == -1) {
-        // if no non-whitespace characters were found, we just want the length of the all-whitespace string
-        // it is sometimes the case that there are whitespace characters in the first text node, sometimes not.
-        firstNonWhitespace = codeSegment.length;
+       tr.appendChild(descTd);
+       tr.appendChild(valueTd);
+       resultsTable.appendChild(tr);
+     });
+     headerPane.appendChild(resultsTable);
+  },
+
+  /** Gets the appropriate div ID for the panel
+   *  where the results will be displayed.
+   *  Implemented as a function to handle different div IDs
+   *  for different versions of the UI.
+   *
+   * @private
+   * @return {String} the ID to use for finding the header pane
+   */
+  getPRPanelId_: function() {
+    return 'partial-discussion-header';
+  },
+
+  /** Returns the various URL components of the passed-in URL.
+   *
+   * @private
+   * @param {String} the URL to parse
+   * @return {Array} the components of the URL split on /
+   */
+  splitUrl_: function(url) {
+    return url.split(prisk.constants_.URL_SLASH);
+  },
+
+  constants_: {
+    URL_SLASH: '/',
+    RESULTS_ID: 'prisk-overall-risk-results',
+    MINIMAL_SEARCH_RESULTS: '&per_page=1',
+    FIRST_NON_WHITESPACE_REGEX: /[^\s]/,
+
+    FIELD_TO_DESCRIPTION: {
+      // Sets up the details for each risk, keyed by the ID of the field that
+      // will be populated.
+
+      // Note that for newness, the values represent
+      // 100 (max percentage) - the risk factor
+      // so that we can use the same logic here
+      // as elsewhere (value <= goodValue = GOOD )
+      AUTHOR_NEWNESS: {
+        id: 'prisk-author-newness-risk',
+        description: 'Author experience risk',
+        goodValue: 80,
+        warnValue: 90
+      },
+
+      AVG_MAX_COMPLEXITY: {
+        id: 'pr-buddy-avg-max-complexity',
+        description: 'Average max complexity risk',
+        goodValue: 2,
+        warnValue: 4
+      },
+
+      NUM_FILES: {
+        id: 'pr-buddy-num-files',
+        description: 'Number of files risk',
+        goodValue: 10,
+        warnValue: 15
+      },
+
+      TOTAL_CHANGES: {
+        id: 'pr-buddy-total-changes',
+        description: 'Total changes risk',
+        goodValue: 150,
+        warnValue: 215
       }
-      return firstNonWhitespace;
-  });
+    }
 
-  // now get the unique values of indents within this diff. the length of that
-  // is the maximum complexity of the diff's additions
-  var uniqueIndents = [];
-  individualIndents.forEach( function(indent) {
-    // use "" + indent because 0.toString seems to yield 0, not "0"
-    uniqueIndents[" " + indent] = true;
-  });
-  // note that you've made an associative array, which is an Object
-  // so you need the length of keys to get the length of the "array"
-  return Object.keys(uniqueIndents).length;
-});
+  }
+};
 
-// exclude diffs that have 0 additions (all deletions)
-// because they skew the results
-var nonZeroComplexities = maxComplexities.filter( function(complexity) { return complexity != 0;} );
-var nonZeroComplexitiesSum = nonZeroComplexities.reduce( function( prev, current, _index, _array) { return prev + current; }, 0 );
-var complexityCell = document.getElementById(AVG_MAX_COMPLEXITY_ID);
-replaceFirstChildWithText(complexityCell, nonZeroComplexitiesSum/nonZeroComplexities.length);
-
-// TODO: the real way to do this is to get the PR blob and then kick off dependent tasks versus getting it twice
-getPR(window.location.href, function(prData) {
-  console.log(prData);
-  replaceFirstChildWithText(document.getElementById(TOTAL_CHANGES_ID), (prData.additions + prData.deletions).toString());
-});
+prisk.createRiskAssessment();
